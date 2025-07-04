be055d3938e845571eaa3ffe1437dcf73ffac29f
Daniel Simuyu Murunga <124144590+murungadaniel@users.noreply.github.com>
2025-07-04 15:48:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-import tensorflow as tf
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
+# import tensorflow as tf
+# from tensorflow.keras.models import load_model
+# from tensorflow.keras.preprocessing import image
 import numpy as np
 import pandas as pd
 import os
